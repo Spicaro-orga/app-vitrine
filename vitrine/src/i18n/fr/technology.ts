fc889acc3c5c03807d74d582598ff0c01201a50c
ThibsC <tiboclement@gmail.com>
2025-09-04 15:19:37 +0200

    Update technology.ts

--- vitrine/src/i18n/fr/technology.ts
+++ vitrine/src/i18n/fr/technology.ts
@@ -1,19 +1,20 @@
 const technology = {
   title: 'La technologie Spicaro',
-  description: `Nous digitalisons vos œuvres d’Art grâce à notre <strong>set photographique portatif.</strong> Nous compilons les documents inhérents à l’œuvre et créons son <strong>jumeau numérique.</strong> Visualisez votre collection via notre plateforme Spicaro sous format <strong>catalogue, galerie 3D, casque de réalité virtuelle</strong> et <strong>réalité augmentée.</strong>`,
+  description: `Nous digitalisons vos œuvres d’Art grâce à notre <strong>set photographique portatif.</strong> Nous compilons les documents inhérents à l’œuvre et créons son <strong>jumeau numérique.</strong>`,
 
   artwork: {
-    title: 'L’Œuvre',
+    title: 'L’œuvre',
     photography:
       'Photographie ultra haute définition, permettant la visualisation de chaque détail',
-    digitalTwin: 'Poinçon infalsifiable matérialisant le jumeau numérique',
+    digitalTwin: 'Marquage matérialisant le jumeau numérique',
     analysis:
       'Analyse scientifique pour une classification et une cartographie des pigments',
     uvLight:
-      "Balayage spectral de l'uv à l'infrarouge pour connaître toute l'histoire cachée",
+      "Balayage spectral de l'uv à l'infrarouge pour découvrir toute l'histoire cachée",
     items: [
       'Photographie ultra haute définition, permettant la visualisation de chaque détail',
-      'Poinçon infalsifiable matérialisant le jumeau numérique',
+      
+      'Marquage matérialisant le jumeau numérique',
 
       'Analyse scientifique pour une classification et une cartographie des pigments',
 
@@ -21,11 +22,11 @@ const technology = {
     ],
   },
   platform: {
-    title: 'La platforme',
+    title: 'La plateforme',
     items: [
       'Accédez à l’ensemble de votre collection, <strong>où que vous soyez</strong>, d’un simple clic.',
 
-      'Retrouvez toutes vos Œuvres, <strong>filtrez</strong> et <strong>triez</strong> les selon vos critères.',
+      'Retrouvez toutes vos œuvres, <strong>filtrez</strong> et <strong>triez</strong> les selon vos critères.',
 
       'Consultez tous les documents, vidéos et audios, sur notre plateforme, via <strong>votre espace sécurisé.</strong>',
     ],
@@ -33,11 +34,11 @@ const technology = {
   experience: {
     title: 'L’experience',
     items: [
-      'Profitez de vos Œuvres grâce à notre <strong>galerie 3D</strong>, agencez vos tableaux selon vos désirs.',
+      'Profitez de vos œuvres grâce à notre <strong>galerie 3D</strong>, agencez vos tableaux selon vos désirs.',
 
-      'Vivez l’expérience en immersif avec votre <strong>casque de réalité augmentée.</strong>',
+      'Vivez l’expérience en immersif avec votre <strong>casque de réalité virtuelle.</strong>',
 
-      'Préparez l’installation de vos Œuvres grâce à la <strong>réalité virtuelle</strong>, dans votre intérieur.',
+      'Visualisez l’installation de vos œuvres grâce à la <strong>réalité augmantée</strong>, dans votre intérieur.',
 
       'Créez une exposition de votre collection afin de <strong>la partager.</strong>',
     ],
