dc761b39b5b4cbdd64910662cf6fbb6e5eba4863
ThibsC <tiboclement@gmail.com>
2025-09-04 15:34:36 +0200

    Update home.ts

--- vitrine/src/i18n/fr/home.ts
+++ vitrine/src/i18n/fr/home.ts
@@ -1,5 +1,5 @@
 const home = {
-  title: 'Spicaro vous accompagne avec sérénité ',
+  title: 'Spicaro vous accompagne avec sérénité',
   description:
     'Spicaro propose un répertoire innovant et sécurisé pour garantir la propriété des œuvres d’Art via la création d’un jumeau numérique.</strong>',
   search: 'Chercher une œuvre, une galerie, un artiste',
